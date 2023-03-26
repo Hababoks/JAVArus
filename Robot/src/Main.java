76dc28d93b322e0abfb5bfbee4b4444b36e293a3
Hababoks <126653941+Hababoks@users.noreply.github.com>
2023-03-26 23:36:39 +0300

    Add files via upload
    five easy HW

---
+++ Robot/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int id = 25123;
+        int age = 34;
+        String documents = " Passport ";
+        String behavior = " Character ";
+
+    }
+}
---
+++ toothcounter/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int toothCounter = 32;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        System.out.println(toothCounter);
+    }
+}
